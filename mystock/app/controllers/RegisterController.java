9517e7a703703d0ca341fbcc639d343577cfc5b4
nrowley <nelsonononivu2002@gmail.com>
2020-07-30 15:56:07 +0100

    removed intellij files

--- mystock/app/controllers/RegisterController.java
+++ mystock/app/controllers/RegisterController.java
@@ -1,18 +1,21 @@
 package controllers;
 
+import play.data.*;
+import javax.inject.*;
 import play.mvc.*;
 import java.util.*;
 
 public class RegisterController extends Controller {
 
+    @Inject FormFactory formFactory;
 
     public Result register() {
         return ok(views.html.register.render());
 
     }
 
-    public Result submitDetails(){
-        // TODO: 27/07/2020
-        return(ok("todo"));
+    public Result submitDetails(Http.Request request){
+        DynamicForm requestData = formFactory.form().bindFromRequest(request);
+        return ok(requestData.get("username"));
     }
 }
