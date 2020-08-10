341102d14b0f9c7a8551e24ea2c4b90d53742b16
nrowley <nelsonononivu2002@gmail.com>
2020-08-10 11:10:30 +0100

    change build.sbt

--- mystock/app/controllers/Register.java
+++ mystock/app/controllers/Register.java
@@ -3,6 +3,8 @@ package controllers;
 import play.*;
 import models.User;
 import models.DbHandler;
+import javax.inject.*;
+
 
 public class Register extends Controller {
 
@@ -17,7 +19,6 @@ public class Register extends Controller {
     public Result submitDetails(Http.Request request){
         
  
-
         Form<User> userForm = formFactory.form(User.class).withDirectFieldAccess(true);
         User user = userForm.bindFromRequest(request).get();
         user.hashPassword(); //hash user password
--- mystock/build.sbt
+++ mystock/build.sbt
@@ -4,11 +4,12 @@ organization := "com.nrowley"
 version := "1.0-SNAPSHOT"
 
 
-lazy val root = (project in file(".")).enablePlugins(PlayScala)
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
 
 scalaVersion := "2.13.3"
 
 libraryDependencies += guice
 libraryDependencies += javaForms
 libraryDependencies += "com.datastax.oss" % "java-driver-core" % "4.0.0"
-enablePlugins(JettyPlugin)
+
+
